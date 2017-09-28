c0f6fd4aba06d298ad95e5785d73fbb164fde42a
emwonder <emwonder@users.noreply.github.com>
2017-09-29 01:19:30 +0300

    3

--- components/app/app.js
+++ components/app/app.js
@@ -1,20 +1,47 @@
-(function () {
-  'use strict'
+(function() {
+    'use strict';
 
-  // import
-  const Menu = window.Menu
+    // import
+    const Menu = window.Menu;
 
-  class App {
-    constructor ({el}) {
-      this.menu = new Menu({
-        el: el.querySelector('.js-menu'),
-        data: {
-          title: 'Сайты',
-          items: []
+    class App {
+        constructor ({el}) {
+
+            this.menu = new Menu({
+                el: el.querySelector('.js-menu'),
+                data: {
+                    title: 'Сайты',
+                    items: []
+                }
+            });
+
+            this.menu.setData({
+                title: 'Сайты',
+                items: [
+                    {title: 'Первый'},
+                    {
+                      title: 'Второй',
+                      items: [
+                        {title: 'Второй-первый'},  
+                        {
+                          title: 'Второй-второй',
+                          items: [
+                            {title: 'Второй-второй-1'},
+                            {title: 'Второй-второй-2'},
+                            {title: 'Второй-второй-3'}
+                          ]
+                        },  
+                      ]
+                    },
+                    {title: 'Третий'},
+                    {title: 'Четвертый'}
+                  ]
+            });
+
         }
-      })
     }
-  };
 
-  window.App = App
-})()
+    // export 
+     window.App = App;
+
+})();
