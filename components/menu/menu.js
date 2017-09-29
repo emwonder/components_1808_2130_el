819387df524ef417a8fa32ae2727bba93dffff71
rulad <rulad@RUMOL0389>
2017-09-29 23:47:14 +0300

    upd

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,152 +1,156 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  /**
-   * @typedef {Item} Тип элемента меню
-   * @prop {string} href URL
-   * @prop {string} anchor текст
-   */
+    const tmpl = window.menuTmpl;
 
-  /**
-   * Компонента "Меню"
-   */
-  class Menu {
-      /**
-       * @constructor
-       * @param  {Object} opts
-       */
-      constructor(opts) {
-          this.el = opts.el;
-          this.data = opts.data;
-          this.onPick = opts.onPick;
+    /**
+     * @typedef {Item} Тип элемента меню
+     * @prop {string} href URL
+     * @prop {string} anchor текст
+     */
 
-          this.render();
-          this._initEvents();
-      }
+    /**
+     * Компонента "Меню"
+     */
+    class Menu {
+        /**
+         * @constructor
+         * @param  {Object} opts
+         */
+        constructor(opts) {
+            this.el = opts.el;
+            this.data = opts.data;
+            this.onPick = opts.onPick;
 
-      /**
-       * Добавляем элемент меню
-       * @param {Object} item
-       */
-      addItem(item) {
-          let el = document.createElement('div');
-          el.innerHTML = this.getItemHtml(item, this.data.items.length);
-          el = el.firstElementChild;
+            this.render();
+            this._initEvents();
+        }
 
-          this.list.append(el);
-          el.addEventListener('animationend', () => el.classList.remove('bounce-in-left'));
-          el.classList.add('bounce-in-left');
+        /**
+         * Добавляем элемент меню
+         * @param {Object} item
+         */
+        addItem(item) {
+            let el = document.createElement('div');
+            el.innerHTML = this.getItemHtml(item, this.data.items.length);
+            el = el.firstElementChild;
 
-          this.data.items.push(item);
-      }
+            this.list.append(el);
+            el.addEventListener('animationend', () => el.classList.remove('bounce-in-left'));
+            el.classList.add('bounce-in-left');
 
-      /**
-       * Удаляем пункт меню из данных
-       * @param  {Object} removedItem
-       */
-      removeItem(removedItem) {
-          this.data.items = this.data.items.filter((item, index) => {
-              return index !== removedItem.index;
-          });
-          this.render();
-      }
+            this.data.items.push(item);
+        }
 
-      /**
-       * HTML одного объекта меню
-       * @param {Item} item
-       * @param {number} index
-       * @return {string}
-       */
-      getItemHtml(item, index) {
-          return `
-          <li class="pure-menu-item" data-index="${index}">
-              <a 
-              class="pure-menu-link"
-              href="${item.href}"
-              data-action="pick">
-                  ${item.anchor}
-              </a>
-              <i class="close" data-action="remove"></i>
-          </li>`;
-      }
+        /**
+         * Удаляем пункт меню из данных
+         * @param  {Object} removedItem
+         */
+        removeItem(removedItem) {
+            this.data.items = this.data.items.filter((item, index) => {
+                return index !== removedItem.index;
+            });
+            this.render();
+        }
 
-      /**
-       * Создаем HTML
-       */
-      render() {
-          /**
-           * Создаем HTML элементов меню
-           * @param {Array<Item>} itmes
-           * @return {string}
-           */
-          let generateItems = (itmes) => {
-              return itmes.map(this.getItemHtml.bind(this)).join('');
-          };
+        /**
+         * HTML одного объекта меню
+         * @param {Item} item
+         * @param {number} index
+         * @return {string}
+         */
+        getItemHtml(item, index) {
+            return `
+            <li class="pure-menu-item" data-index="${index}">
+                <a 
+                class="pure-menu-link"
+                href="${item.href}"
+                data-action="pick">
+                    ${item.anchor}
+                </a>
+                <i class="close" data-action="remove"></i>
+            </li>`;
+        }
 
-          this.el.innerHTML = `
-          <div class="menu pure-menu custom-restricted-width">
-              <span class="menu__title pure-menu-heading">
-              ${this.data.title}
-              </span>
-              <ul class="menu__list pure-menu-list">
-              ${generateItems(this.data.items)}
-              </ul>
-          </div>
-          `;
+        /**
+         * Создаем HTML
+         */
+        render() {
+            /**
+             * Создаем HTML элементов меню
+             * @param {Array<Item>} itmes
+             * @return {string}
+             */
+            let generateItems = (itmes) => {
+                return itmes.map(this.getItemHtml.bind(this)).join('');
+            };
 
-          this.list = this.el.querySelector('.menu__list');
-          this.title = this.el.querySelector('.menu__title');
-      }
+            this.el.innerHTML = tmpl(this.data);
 
-      /**
-      * Удаления элемента меню
-      * @param  {Element} item
-      * @private
-      */
-      _onRemoveClick(item) {
-          let el = /** @type {Element} */ item.parentNode;
+            `
+            <div class="menu pure-menu custom-restricted-width">
+                <span class="menu__title pure-menu-heading">
+                ${this.data.title}
+                </span>
+                <ul class="menu__list pure-menu-list">
+                ${generateItems(this.data.items)}
+                </ul>
+            </div>
+            `;
 
-          let index = parseInt(item.parentNode.dataset.index, 10);
-          el.addEventListener('animationend', this.removeItem.bind(this, {index}));
-          el.classList.add('bounce-out-right');
-      }
+            this.list = this.el.querySelector('.menu__list');
+            this.title = this.el.querySelector('.menu__title');
+        }
 
-      /**
-      * Выбор элемента меню
-      * @param  {HTMLElement} item
-      */
-      _onPickClick(item) {
-          this.onPick(item);
-      }
+        /**
+        * Удаления элемента меню
+        * @param  {Element} item
+        * @private
+        */
+        _onRemoveClick(item) {
+            let el = /** @type {Element} */ item.parentNode;
 
-      /**
-      * Развешиваем события
-      */
-      _initEvents() {
-          this.el.addEventListener('click', this._onClick.bind(this));
-      }
+            let index = parseInt(item.parentNode.dataset.index, 10);
+            el.addEventListener('animationend', this.removeItem.bind(this, {index}));
+            el.classList.add('bounce-out-right');
+        }
 
-      /**
-      * Клик в любую область меню
-      * @param {Event} event
-      * @private
-      */
-      _onClick(event) {
-          event.preventDefault();
-          let item = event.target;
+        /**
+        * Выбор элемента меню
+        * @param  {HTMLElement} item
+        */
+        _onPickClick(item) {
+            this.onPick(item);
+        }
 
-          switch (item.dataset.action) {
-          case 'remove':
-          this._onRemoveClick(item);
-          break;
+        /**
+        * Развешиваем события
+        */
+        _initEvents() {
+            this.el.addEventListener('click', this._onClick.bind(this));
+        }
 
-          case 'pick':
-          this._onPickClick(item);
-          break;
-          }
-      }
-  }
+        /**
+        * Клик в любую область меню
+        * @param {Event} event
+        * @private
+        */
+        _onClick(event) {
+            event.preventDefault();
+            let item = event.target;
 
-  // Export
-  window.Menu = Menu;
+            switch (item.dataset.action) {
+            case 'remove':
+            this._onRemoveClick(item);
+            break;
+
+            case 'pick':
+            this._onPickClick(item);
+            break;
+            }
+        }
+    }
+
+    // Export
+    window.Menu = Menu;
 })(window);
